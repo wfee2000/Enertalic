029ec366d1426a1c0170caac1a6cd276f5532b37
wfee2000 <1winnie@protonmail.com>
2026-01-17 17:37:28 +0100

    Inital Commit

---
+++ src/main/java/com/wfee/EnertalicPlugin.java
@@ -0,0 +1,12 @@
+package com.wfee;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class EnertalicPlugin extends JavaPlugin {
+    public EnertalicPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+}
