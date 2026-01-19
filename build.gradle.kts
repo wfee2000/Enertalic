0e3e6e0cb4ba90b03081131bfb815c561083fefe
wfee2000 <1winnie@protonmail.com>
2026-01-19 23:01:22 +0100

    feat: improve and bundle gradle config and plugin config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,51 @@
-import kotlin.time.Clock
-
 plugins {
     id("java")
 }
 
 group = "com.wfee"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
-val hytalePath = System.getenv("HOME") + "/.local/share"
-val installation = "$hytalePath/Hytale/install/release/package/game/latest"
-val serverFile = file("$installation/Server/HytaleServer.jar")
+val hytalePath = System.getenv("HOME") + "/.local/share/Hytale"
+val installation = "$hytalePath/install/release/package/game/latest"
+val serverFile = "$installation/Server/HytaleServer.jar"
+val modPath = "$hytalePath/UserData/Mods"
+
 
 dependencies {
-    if ((serverFile).exists()) {
+    if (file(installation).exists()) {
         compileOnly(files(serverFile))
     } else {
-        logger.error("Hytale Server not found! ${serverFile.absolutePath}")
+        logger.error("Hytale Installation not found! ${file(installation).absolutePath}")
     }
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+tasks.named<ProcessResources>("processResources") {
+    var replaceProperties = mapOf(
+        "group" to project.group,
+        "version" to project.version,
+        "main_entrypoint" to "${project.group}.${rootProject.name}",
+        "name" to rootProject.name,
+        "author" to findProperty("author")
+    )
+
+    filesMatching("manifest.json") {
+        expand(replaceProperties)
+    }
+
+    inputs.properties(replaceProperties)
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    destinationDirectory.set(file(modPath))
 }
