195d2ce8b02ed9bfb9a1652372c2c97fecb96c7d
wfee2000 <1winnie@protonmail.com>
2026-03-03 10:45:29 +0100

    feat: add methods to direction

--- src/main/java/com/wfee/enertalic/util/Direction.java
+++ src/main/java/com/wfee/enertalic/util/Direction.java
@@ -1,14 +1,16 @@
 package com.wfee.enertalic.util;
 
 import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.server.core.asset.type.blocktype.config.Rotation;
+import com.hypixel.hytale.server.core.asset.type.blocktype.config.RotationTuple;
 
 public enum Direction {
-    East(new Vector3i(1,0,0)),
-    West(new Vector3i(-1,0,0)),
-    Up(new Vector3i(0,1,0)),
-    Down(new Vector3i(0,-1,0)),
-    South(new Vector3i(0,0,1)),
-    North(new Vector3i(0,0,-1));
+    East(Vector3i.EAST),
+    West(Vector3i.WEST),
+    Up(Vector3i.UP),
+    Down(Vector3i.DOWN),
+    South(Vector3i.SOUTH),
+    North(Vector3i.NORTH);
 
     private final Vector3i offset;
 
@@ -28,6 +30,26 @@ public enum Direction {
             case Down -> Up;
             case South -> North;
             case North -> South;
+        };
+    }
+
+    public RotationTuple getRotation() {
+        return switch (this) {
+            case North -> RotationTuple.NONE;
+            case West -> RotationTuple.of(Rotation.Ninety,  Rotation.None);
+            case South -> RotationTuple.of(Rotation.OneEighty, Rotation.None);
+            case East -> RotationTuple.of(Rotation.TwoSeventy, Rotation.None);
+            case Up -> RotationTuple.of(Rotation.None, Rotation.Ninety);
+            case Down -> RotationTuple.of(Rotation.None, Rotation.TwoSeventy);
+        };
+    }
+
+    public Direction getLeft() {
+        return switch (this) {
+            case North, Up -> West;
+            case West -> South;
+            case South, Down -> East;
+            case East -> North;
         };
     }
 }
